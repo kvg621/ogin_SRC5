0acaf630b4eddf72d307abb916cf0cca23dd9540
kvg621 <konst000@yandex.ru>
2021-11-06 09:52:42 +0400

    Вывод перенесен из println в Log.d

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,35 +11,45 @@ val TAGS:String="DEBUG_SYS"
     override fun onCreate(savedInstanceState: Bundle?) {
               super.onCreate(savedInstanceState)
         setContentView(main_activity)
-println("Однажды, в студеную зимнюю пору")
+        Log.d(TAGS,"Однажды, в студеную зимнюю пору")
+
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(TAGS,"OnResume")
-        println("Гляжу, поднимается медленно в гору")
+        Log.d(TAGS,"Гляжу, поднимается медленно в гору")
+
     }
     override fun onStart() {
         super.onStart()
-        Log.d(TAGS,"OnStart")
-        println("Я из лесу вышел; был сильный мороз.")
+        Log.d(TAGS,"Я из лесу вышел; был сильный мороз.")
+
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d(TAGS,"OnStop")
-        println("Лошадка, везущая хворосту воз.")
+        Log.d(TAGS,"И шествуя важно, в спокойствии чинном,")
+
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(TAGS,"OnPause")
-        println("И шествуя важно, в спокойствии чинном,")
+        Log.d(TAGS,"Лошадка, везущая хворосту воз.")
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(TAGS,"OnDestroy")
-        println("Лошадку ведет под уздцы мужичок")
+        Log.d(TAGS,"Лошадку ведет под уздцы мужичок")
+
     }
 }
+/*
+Однажды, в студеную зимнюю пору
+Я из лесу вышел; был сильный мороз.
+Гляжу, поднимается медленно в гору
+Лошадка, везущая хворосту воз.
+И шествуя важно, в спокойствии чинном,
+Лошадку ведет под уздцы мужичок
+
+*/
